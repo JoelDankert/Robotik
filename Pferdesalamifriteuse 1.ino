ca6bdf1f04d3af22f4b1d0290b08f24621fd08e7
Joel Dankert <106259863+JoelDankert@users.noreply.github.com>
2023-02-15 14:49:51 +0100

    Update Pferdesalamifriteuse 1.ino

--- Pferdesalamifriteuse 1.ino
+++ Pferdesalamifriteuse 1.ino
@@ -2,7 +2,48 @@
 
 // following functions for sensors
 
+int getdistfront(){
+
+
+}
+int getdistright(){
+
+
+}
+int getdistback(){
+
+
+}
 int getdistleft(){
 
 
+}
+
+int getdist(int direction){
+  if (direction == 0){
+    return getdistfront();
+  }
+  if (direction == 1){
+    return getdistright();
+  }
+  if (direction == 2){
+    return getdistback();
+  }
+  if (direction == 3){
+    return getdistleft();
+  }
+
+}
+
+
+// following functions connect the sensors relatively to looking position
+
+int getrelativedist(int relativesensor, int direction){ //0=front 1=right 2=back 3=left
+  int directionnew = relativesensor + direction;
+  if (directionnew > 3){
+    directionnew -= 3
+  }
+  
+  
+  
 }
