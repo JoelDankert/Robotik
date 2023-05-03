41792d886389dbe3ae398b17e5fd35e2d7bc75aa
Joel Dankert <106259863+JoelDankert@users.noreply.github.com>
2023-05-03 14:59:09 +0200

    Create test.cc

---
+++ slavik/test.cc
@@ -0,0 +1,30 @@
+#define motorPinL1 2
+#define motorPinL2 3
+#define motorPinR1 4
+#define motorPinR2 5
+#define motorSpeedL 9
+#define motorSpeedR 10
+
+void setuppins()
+{
+  pinMode(motorPinL1, OUTPUT);
+  pinMode(motorPinL2, OUTPUT);
+  pinMode(motorPinR1, OUTPUT);
+  pinMode(motorPinR2, OUTPUT);
+  //pinMode(motorSpeedL,OUTPUT);
+  //pinMode(motorSpeedR,OUTPUT);
+}
+
+void Test()
+{
+  digitalWrite(motorPinL1, LOW);
+  digitalWrite(motorPinL2, HIGH);
+  digitalWrite(motorPinR1, LOW);
+  digitalWrite(motorPinR2, HIGH);
+}
+
+void setup(){
+  Serial.begin(9600);
+  Test()
+  
+}
