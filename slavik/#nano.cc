f1510c58ec07c7ed8fe9eaa7e075d785c5deda61
Joel Dankert <106259863+JoelDankert@users.noreply.github.com>
2024-04-18 23:54:48 +0200

    Update #nano.cc

--- slavik/#nano.cc
+++ slavik/#nano.cc
@@ -84,11 +84,10 @@ void loop() {
       unchangedTicks++;
       lastTickState = currentTickState;
     } else {
-      // Reset the counter if the tick state has changed
       unchangedTicks = 0;
       lastTickState = currentTickState;
     }
-    if (unchangedTicks >= 30) {
+    if (unchangedTicks >= 20) {
       Serial.println("Tick signal stopped. Resetting Mega...");
       digitalWrite(resetmega, LOW); // Assuming LOW triggers a reset
       delay(100); // Hold the reset for 100ms
@@ -98,7 +97,7 @@ void loop() {
     }
   
   }
-  delay(50);
+  delay(25);
 }
 
 
